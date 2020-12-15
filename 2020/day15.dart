25223477b01684d06baed2f7e33583a527e0d6cd
Jess <castaway@desert-island.me.uk>
2020-12-15 09:29:20 +0000

    Day 15 part 1

---
+++ 2020/day15.dart
@@ -0,0 +1,50 @@
+class Day15 {
+  static var dayFile='2020/day15.txt';
+
+  static Map<int, List<int>> numbers = {};
+  static int lastNum = -1;
+  static int nextIt = 0;
+  static handle(String line) {
+    var ints = line.split(',');
+    for(var i=0; i < ints.length; i++) {
+      var val = int.parse(ints[i]);
+      //print(val);
+      numbers[val] = [];
+      numbers[val].add(i);
+      lastNum = val;
+      nextIt = i;
+    }
+  }
+
+  static whenDone() {
+    print(numbers);
+    for(var i = nextIt+1; i < 2020; i++) {
+      iterate(i);
+      nextIt=i;
+      print('$i, $lastNum');
+    }
+    // print(numbers);
+    print('PART 1: $lastNum');
+    /*
+    for(var i = nextIt+1; i <  30000000; i++) {
+      iterate(i);
+      //print('$i, $lastNum');
+    }
+    */
+    print('PART 2: $lastNum');
+  }
+
+  static iterate(int iteration) {
+    // did we see this before?
+    if(numbers[lastNum].length == 1) {
+      lastNum = 0;
+    } else {
+      var numList = numbers[lastNum];
+      lastNum = numList.last - numList[numList.length-2];
+    }
+    if(!numbers.containsKey(lastNum)) {
+      numbers[lastNum] = [];
+    }
+    numbers[lastNum].add(iteration);
+  }
+}
--- main.dart
+++ main.dart
@@ -14,20 +14,21 @@ import './2020/day10.dart';
 import './2020/day11.dart';
 import './2020/day12.dart';
 import './2020/day13.dart';
-import './2020/day14.dart';
+// import './2020/day14.dart';
+import './2020/day15.dart';
 
 void main(List<String> arguments) {
   
-  var filename = Day14.dayFile; // arguments[0];
+  var filename = Day15.dayFile; // arguments[0];
   File inputFile = new File(filename);
   Stream<List<int>> inputStream = inputFile.openRead();
   inputStream
   .transform(utf8.decoder)
   .transform(new LineSplitter())
   .listen((String line) {
-    Day14.handle(line);
+    Day15.handle(line);
     },
-    onDone: () { Day14.whenDone(); },
+    onDone: () { Day15.whenDone(); },
     onError: (e) { print(e.toString()); }
   );
 //  print(total);
